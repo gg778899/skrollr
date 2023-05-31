ea208e99015d055e3ec2760543ebe704cf8b1ea8
gg778899 <e125438701@gmail.com>
2023-05-31 20:59:07 +0800

    nothing

---
+++ script/dist/script.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var skrollrInstance = skrollr.init();
+$(window).scroll(function (e) {
+  if ($(window).scrollTop() > 0) {
+    $("nav").css("background-color", "#FCDD9C");
+  } else $("nav").css("background-color", "rgba(0, 0, 0, 0)");
+});
+$(window).resize(function () {
+  skrollrInstance.refresh();
+});
